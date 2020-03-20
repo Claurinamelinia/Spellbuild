5db1257e6b4c94612178bcb6326f7e8c5d1aadca
Claurinamelinia <claurina.triatma@gmail.com>
2020-03-20 12:07:47 +0700

    add spellbuild, design, and fuction

---
+++ function.js
@@ -0,0 +1,5 @@
+// function spellbuild(){
+//     document.getElementById("greeting").style.transitionTimingFunction = "ease";
+//     document.getElementById("greeting").style.transitionDuration = "10s";
+// }
+
